66140437cdba0cf26036a4a6d2d4d865bdd039de
pangadev <pgacek0@gmail.com>
2020-04-21 22:58:22 +0200

    commented out

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {Switch, Route} from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navigation from './components/Navigation'
