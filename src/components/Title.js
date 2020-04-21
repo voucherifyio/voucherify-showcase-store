5ff5e7febddc51edff4ac217ea88613323def9b9
pangadev <pgacek0@gmail.com>
2020-04-21 22:52:13 +0200

    Updated title component

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Title({name, title}) {
     return (
-        <div className="row">
+        <div className="row my-4">
             <div className="col-10 mx-auto my-2 text-center">
-                <h1 className="text-capitalize">
+                <h1>
                     {name} {title}
                 </h1>
             </div>
