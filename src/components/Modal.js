6a4d5955f25f4f60f1475eb8c4e2fc7b64120d9e
pangadev <pgacek0@gmail.com>
2020-04-21 22:56:16 +0200

    Changes to Modal Component - it may be useful later

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react'
 import {ProductConsumer} from './Context'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
+
+// This will be used for pop-up banners
 
 export default class Modal extends Component {
     render() {
         return (
             <ProductConsumer>
                 {(value) => {
-                    const {modalOpen, closeModal} = value;
-                    const {img, title, price} = value.modalProduct
+                    // const {modalOpen, closeModal} = value;
+                    // const {img, title, price} = value.modalProduct
 
-                    if (!modalOpen) {
-                        return null;
-                    }
-                    else {
-                        return (
-                            <div>
-                            </div>
-                        )
-                    }
+                    // if (!modalOpen) {
+                    //     return null;
+                    // }
+                    // else {
+                    //     return (
+                    //         <div>
+                    //         </div>
+                    //     )
+                    // }
                 }}
             </ProductConsumer>
         )
