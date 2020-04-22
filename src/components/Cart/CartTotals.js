4845d2aa330abe9d6fb068a9e598ac259906e357
pangadev <pgacek0@gmail.com>
2020-04-22 15:52:59 +0200

    Removed useless lines

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function CartTotals({ value }) {
+  const {
+    cartSubTotal,
+    cartTax,
+    cartTotal,
+    clearCart,
+    cartDiscount,
+    cartTotalAfterPromotion,
+    promotionItemsNumber,
+  } = value;
+
   return (
     <div className='col-lg-6'>
       <div className='bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold'>
@@ -11,53 +21,52 @@ export default function CartTotals({ value }) {
         <ul className='list-unstyled mb-4'>
           <li className='d-flex justify-content-between py-3 border-bottom'>
             <strong className='text-muted'>Subtotal </strong>
-            <strong>${value.cartSubTotal}</strong>
+            <strong>${cartSubTotal}</strong>
           </li>
           <li className='d-flex justify-content-between py-3 border-bottom'>
             <strong className='text-muted'>Tax</strong>
-            <strong>${value.cartTax}</strong>
+            <strong>${cartTax}</strong>
           </li>
           <li className='d-flex justify-content-between py-3 border-bottom'>
             <h5>
               <strong className='text-muted'>Total</strong>
             </h5>
-            <h5 className='font-weight-bold'>${value.cartTotal}</h5>
+            <h5 className='font-weight-bold'>${cartTotal}</h5>
           </li>
-          {(value) => {
-            const { promotionItemsNumber } = value;
-            if (promotionItemsNumber > 0) {
-              return (
-                <>
-                  <li className='d-flex justify-content-between py-3 border-bottom'>
-                    <strong className='text-success'>Discount</strong>
-                    <strong className='text-success'>
-                      - ${value.cartDiscount}
-                    </strong>
-                  </li>
-                  <li className='d-flex justify-content-between py-3 border-bottom'>
-                    <h5>
-                      <strong className='text-success'>After discount</strong>
-                    </h5>
-                    <h5 className='font-weight-bold text-success'>
-                      ${value.cartTotalAfterPromotion}
-                    </h5>
-                  </li>
-                </>
-              );
-            }
-          }}
+          {promotionItemsNumber > 0 && (
+            <>
+              <li className='d-flex justify-content-between py-3 border-bottom'>
+                <strong className='text-success'>Discount</strong>
+                <strong className='text-success'>-&nbsp;${cartDiscount}</strong>
+              </li>
+              <li className='d-flex justify-content-between py-3 border-bottom'>
+                <h5>
+                  <strong className='text-success'>After discount</strong>
+                </h5>
+                <h5 className='font-weight-bold text-success'>
+                  ${cartTotalAfterPromotion}
+                </h5>
+              </li>
+            </>
+          )}
         </ul>
-        <Link to='/success' className='link-unstyled' style={{'text-decoration': 'none'}}>
+        <Link
+          to='/success'
+          className='link-unstyled'
+          style={{ 'text-decoration': 'none' }}>
           <button
             className='btn btn-dark rounded-pill py-2 btn-block mb-2 '
-            onClick={() => value.clearCart()}>
+            onClick={() => clearCart()}>
             Proceed to checkout
           </button>
         </Link>
-        <Link to='/' className='link-unstyled' style={{'text-decoration': 'none'}}>
+        <Link
+          to='/'
+          className='link-unstyled'
+          style={{ 'text-decoration': 'none' }}>
           <button
             className='btn btn-danger rounded-pill py-2 btn-block'
-            onClick={() => value.clearCart()}>
+            onClick={() => clearCart()}>
             Clear cart
           </button>
         </Link>
