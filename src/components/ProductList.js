63ca19e522c9b56e748cfe2e095d2a1cdd7746c1
Piotr Gacek <pgacek0@gmail.com>
2020-04-25 11:19:26 +0200

    Removed product Consumer

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,33 +1,23 @@
-import React, { Component } from 'react'
-import Product from './Product'
-import Title from './Title'
-import {ProductConsumer} from './Context'
+import React from "react";
+import Product from "./Product";
+import Title from "./Title";
 import { storeProducts } from "../data";
 
-export default class ProductList extends Component {
-
-    render() {
-        return (
-            <div>
-                <React.Fragment>
-                <div className="py-5">
-                    <div className="container">
-                        <Title name="Our" title="products"></Title>
-                        <div className="row">
-                            <ProductConsumer>
-                                {() => {
-                                    return storeProducts.map(
-                                        product => {
-                                            return <Product key={product.id} product={product}></Product>
-                                        }
-                                    )}
-                                    }
-                            </ProductConsumer>
-                        </div>
-                    </div>
-                </div>
-                </React.Fragment>
+export default function ProductList() {
+  return (
+    <div>
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="Our" title="products"></Title>
+            <div className="row">
+              {storeProducts.map((product) => {
+                return <Product key={product.id} product={product}></Product>;
+              })}
             </div>
-        )
-    }
+          </div>
+        </div>
+      </React.Fragment>
+    </div>
+  );
 }
