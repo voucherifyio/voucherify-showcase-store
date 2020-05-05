7b4f7ff33750ecc9c98fa2965a159a166352b37b
Piotr Gacek <pgacek0@gmail.com>
2020-05-05 11:41:45 +0200

    Styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navigation from './components/Navigation';
-import ProductList from './components/ProductList';
-import Cart from './components/Cart/Cart';
-import Default from './components/Default';
-import Details from './components/Details';
-import Footer from './components/Footer';
-import MainPage from './components/MainPage';
-import SuccessPage from './components/SuccessPage';
-import Sidebar from './components/Sidebar'
-import Modal from './components/Modal';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./components/Navigation";
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart/Cart";
+import Default from "./components/Default";
+import Details from "./components/Details";
+import Footer from "./components/Footer";
+import MainPage from "./components/MainPage";
+import SuccessPage from "./components/SuccessPage";
+import Sidebar from "./components/Sidebar";
+import Modal from "./components/Modal";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const toastOptions = {
-  position: 'bottom-center',
+  position: "bottom-center",
   draggable: false,
-  toastClassName: 'text-xl text-white bg-dark text-center p-3 shadow-none',
-  progressClassName: 'bg-white opacity-25',
+  toastClassName: "text-xl text-white bg-dark text-center p-3 shadow-none",
+  progressClassName: "bg-white opacity-25",
   closeButton: false,
 };
 
@@ -30,11 +30,11 @@ class App extends Component {
         <Sidebar />
         <Navigation />
         <Switch>
-          <Route exact path='/' component={MainPage}></Route>
-          <Route path='/products' component={ProductList}></Route>
-          <Route path='/details/:productId' component={Details}></Route>
-          <Route path='/cart' component={Cart}></Route>
-          <Route path='/success' component={SuccessPage}></Route>
+          <Route exact path="/" component={MainPage}></Route>
+          <Route path="/products" component={ProductList}></Route>
+          <Route path="/details/:productId" component={Details}></Route>
+          <Route path="/cart" component={Cart}></Route>
+          <Route path="/success" component={SuccessPage}></Route>
           <Route component={Default}></Route>
         </Switch>
         <Modal />
