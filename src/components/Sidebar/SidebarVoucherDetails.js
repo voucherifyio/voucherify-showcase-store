a1a0d3038eb5ecf4ba6008e35bd9801a543d7ea4
Piotr Gacek <pgacek0@gmail.com>
2020-08-26 13:00:49 +0200

    Updated Voucher Details

--- src/components/Sidebar/SidebarVoucherDetails.js
+++ src/components/Sidebar/SidebarVoucherDetails.js
@@ -1,13 +1,13 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import Tooltip from '@material-ui/core/Tooltip';
-import {withStyles} from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import PropTypes from 'prop-types';
 
-const SidebarVoucherDetails = ({voucher, code}) => {
+const SidebarVoucherDetails = ({ voucher, code }) => {
   const [open, setOpen] = useState(false);
   const handleTooltipClose = () => {
     setOpen(false);
@@ -18,15 +18,15 @@ const SidebarVoucherDetails = ({voucher, code}) => {
   };
   const VoucherifyButton = withStyles(() => ({
     root: {
-      'color': 'white',
-      'fontFamily': 'Lato',
-      'fontSize': '0.875rem',
-      'backgroundColor': '#ff8b5c',
-      'borderRadius': '20em',
-      'padding': '5px 20px',
-      'marginTop': '15px',
-      'marginBottom': '15px',
-      'textTransform': 'none',
+      color: 'white',
+      fontFamily: 'Lato',
+      fontSize: '0.875rem',
+      backgroundColor: '#ff8b5c',
+      borderRadius: '20em',
+      padding: '5px 20px',
+      marginTop: '15px',
+      marginBottom: '15px',
+      textTransform: 'none',
       '&:hover': {
         backgroundColor: '#ff8b5c',
       },
@@ -35,7 +35,7 @@ const SidebarVoucherDetails = ({voucher, code}) => {
   return (
     <div
       style={{
-        'width': '100%',
+        width: '100%',
         'transition:': 'all .5s ease-in-out',
       }}
       key={voucher.name}
@@ -77,6 +77,23 @@ const SidebarVoucherDetails = ({voucher, code}) => {
             </CopyToClipboard>
           </ClickAwayListener>
         </div>
+        {voucher.discount.amount_limit && (
+          <>
+            <p className="campaign-description section-heading redemption-rules">
+              Voucher information
+            </p>
+            <div className="campaign-step d-flex flex-row align-items-center">
+              <div className="campaign-step-icon">
+                <ArrowRightIcon />
+              </div>
+              <div className="campaign-step-description">
+                <p className="campaign-step-text">
+                  Discount limit: {voucher.discount.amount_limit / 100}
+                </p>
+              </div>
+            </div>
+          </>
+        )}
         {voucher.metadata.demostoreSteps && (
           <>
             <p className="campaign-description section-heading redemption-rules">
@@ -112,7 +129,6 @@ const SidebarVoucherDetails = ({voucher, code}) => {
 };
 
 export default SidebarVoucherDetails;
-
 
 SidebarVoucherDetails.propTypes = {
   voucher: PropTypes.object.isRequired,
