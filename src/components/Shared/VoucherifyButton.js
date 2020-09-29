e2094bff10e8f89f84144ad21889adb580cd7b28
Piotr Gacek <pgacek0@gmail.com>
2020-09-29 18:50:06 +0200

    [Chore] Removed unused dependencies

--- src/components/Shared/VoucherifyButton.js
+++ src/components/Shared/VoucherifyButton.js
@@ -1,46 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Tooltip from '@material-ui/core/Tooltip';
 import PropTypes from 'prop-types';
 
 const VoucherifyButton = ({ code, text, title = 'none', onClickFunction }) => {
-  const [open, setOpen] = useState(false);
-
-  const handleTooltipClose = () => {
-    setOpen(false);
-  };
-
-  const handleTooltipOpen = () => {
-    setOpen(true);
-  };
-
   if (code) {
     return (
-      <ClickAwayListener onClickAway={handleTooltipClose}>
-        <CopyToClipboard text={code}>
-          <Tooltip
-            PopperProps={{
-              disablePortal: true,
-            }}
-            onClose={handleTooltipClose}
-            open={open}
-            disableFocusListener
-            disableHoverListener
-            disableTouchListener
-            title="Copied"
-          >
-            <Button
-              className="voucherifyButton"
-              variant="contained"
-              onClick={handleTooltipOpen}
-            >
-              {!text ? `${code}` : `${text}`}
-            </Button>
-          </Tooltip>
-        </CopyToClipboard>
-      </ClickAwayListener>
+      <Tooltip title="Click to copy">
+        <Button
+          className="voucherifyButton"
+          variant="contained"
+          onClick={() => navigator.clipboard.writeText(code)}
+        >
+          {!text ? `${code}` : `${text}`}
+        </Button>
+      </Tooltip>
     );
   } else if (onClickFunction) {
     return (
