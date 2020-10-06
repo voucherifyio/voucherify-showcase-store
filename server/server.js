8db98639fd87615512713a23e8197e5ee234d7f0
Piotr Gacek <pgacek0@gmail.com>
2020-10-06 11:03:08 +0200

    [Fix] Heroku refresh fix

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
   app.get('/*', (req, res) => {
     // res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
+    res.sendFile(path.resolve(__dirname + '/../build/index.html'));
   });
 } else {
   app.get('/*', (req, res) => {
