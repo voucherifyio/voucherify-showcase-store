30a92e19b89a65cc26056a2d14e355f3723cea0b
Piotr Gacek <pgacek0@gmail.com>
2020-10-06 10:47:39 +0200

    [Fix] Heroku refresh fix

--- server/server.js
+++ server/server.js
@@ -52,12 +52,12 @@ app.use('/distributions', distributionsRouter);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname + '/index.html'));
+    // res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 } else {
   app.get('/*', (req, res) => {
-    // res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    res.sendFile(path.resolve(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
 }
 
